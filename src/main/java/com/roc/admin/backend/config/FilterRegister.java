9293fd7e6a6b76804a0f4ca94b4ba0aa999c4d7a
peng <zhangpeng3710@gmail.com>
2025-04-15 22:56:02 +0800

    quartz optimize

--- src/main/java/com/roc/admin/backend/config/FilterRegister.java
+++ src/main/java/com/roc/admin/backend/config/FilterRegister.java
@@ -4,12 +4,11 @@ package com.roc.admin.backend.config;
 import com.roc.admin.backend.filter.LoginFilter;
 import com.roc.admin.backend.filter.UserOperationRecordFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FilterRegister {
-    @Bean
+    //    @Bean
     public FilterRegistrationBean<LoginFilter> loginFilter() {
         FilterRegistrationBean<LoginFilter> bean = new FilterRegistrationBean<>();
         //-2147483648 is the highest priority, 2147483647 is the lowest priority
@@ -20,7 +19,7 @@ public class FilterRegister {
         return bean;
     }
 
-    @Bean
+    //    @Bean
     public FilterRegistrationBean<UserOperationRecordFilter> userRecordFilter() {
         FilterRegistrationBean<UserOperationRecordFilter> bean = new FilterRegistrationBean<>();
         bean.setOrder(2);
--- src/main/java/com/roc/admin/backend/controller/TimerController.java
+++ src/main/java/com/roc/admin/backend/controller/TimerController.java
@@ -5,13 +5,14 @@ import org.quartz.SchedulerException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Description
  * @Author: Zhang Peng
  * @Date: 2024/6/12
  */
-//@RestController
+@RestController
 @RequestMapping("/timer/test")
 public class TimerController {
 
